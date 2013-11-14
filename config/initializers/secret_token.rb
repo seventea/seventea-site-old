421819099219399375cd5356602218cf0ade83aa
Eric Cohen <eirc.eirc@gmail.com>
2013-11-14 19:17:48 +0200

    Initial commit of `rails new seventea-site --skip-bundle --skip-active-record --skip-test-unit`.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeventeaSite::Application.config.secret_key_base = 'e15ff60dd7064e949bdcf564ca90e6edce34bcd9d7b38a2d5ccd3b8379d93e3ede5746e88850557572f6125ab1dd7141083a7efd096ef741c4cf4c40cecd34d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SeventeaSite::Application.config.session_store :cookie_store, key: '_seventea-site_session'
